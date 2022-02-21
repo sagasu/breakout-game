4cbf06e26b71e16464f2a3a3b3149a3f6052aa33
Mateusz Kopij <sagasu@gmail.com>
2022-02-21 22:38:07 +0000

    working on game.

--- code/game.c
+++ code/game.c
@@ -1,5 +1,39 @@
-#include <stdio.h>
+#include <windows.h>
 
-void main(){
-    printf("Hello World!\n");
+
+LRESULT CALLBACK DefWindowProcA(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam){
+    return DefWindowProcA();
+}
+
+int WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nShowCmd
+){
+
+ WNDCLASSA window_class= {0};
+ window_class.style = CS_HREDRAW | CS_VREDRAW;
+ window_class.lpszClassName = "Game Window";
+ window_class.lpfnWndProc = DefWindowProcA;
+
+//   RegisterClassA(
+//   [in] const WNDCLASSA *lpWndClass
+// );
+
+//   HWND window = HWND CreateWindowExA(
+//   0,
+//   LPCSTR    lpClassName,
+//   LPCSTR    lpWindowName,
+//   DWORD     dwStyle,
+//   int       X,
+//   int       Y,
+//   int       nWidth,
+//   int       nHeight,
+//   HWND      hWndParent,
+//   HMENU     hMenu,
+//   HINSTANCE hInstance,
+//   LPVOID    lpParam
+// );
+  return 0;
 }
