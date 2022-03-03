02f9d3c9791709c7e0656c1d2f58ec69a577d0a8
Mateusz Kopij <sagasu@gmail.com>
2022-03-03 20:03:12 +0000

    displaying window in while loop

--- code/build.bat
+++ code/build.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd ..\build
 echo %CD%
-cl -nologo -Zi ..\code\game.c user32.lib
+cl -nologo -Zi -FC ..\code\game.c user32.lib
 popd
--- code/game.c
+++ code/game.c
@@ -1,5 +1,12 @@
 #include <windows.h>
 
+typedef int b32;
+#define true 1
+#define false 0
+#define global_variable static
+#define internal static
+
+global_variable b32 running = true;
 
 LRESULT window_callback(HWND hWnd,  UINT Msg, WPARAM wParam, LPARAM lParam){
 
@@ -30,5 +37,18 @@ int WinMain(
   WS_VISIBLE | WS_OVERLAPPEDWINDOW,
   CW_USEDEFAULT,CW_USEDEFAULT,
   1280, 720, 0, 0, 0, 0);
+
+
+  while(running){
+    MSG message;
+    while(PeekMessageA(&message, window, 0, 0, PM_REMOVE)){
+      if(message.message == WM_QUIT){
+        running = false;
+      }
+      TranslateMessage(&message);
+      DispatchMessageA(&message);
+    }
+  }
+
 return 0;
 }
